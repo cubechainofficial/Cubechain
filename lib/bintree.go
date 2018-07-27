a4cd2c9a34ff8d1ef2131766f3fb134be3a52aa2
cubechainofficial <void@cubechain.io>
2018-07-27 16:28:01 +0900

    add
    add

--- lib/bintree.go
+++ lib/bintree.go
@@ -8,6 +8,12 @@ type Node struct {
 	right *Node
 }
 
+type BinaryTree struct {
+	root  *Node
+	size  int 
+	order int 
+}
+
 func (n *Node) Left() *Node {
 	return n.left
 }
@@ -32,14 +38,6 @@ func (n *Node) SetValue(val interface{}) {
 	n.value = val
 }
 
-
-
-type BinaryTree struct {
-	root  *Node
-	size  int // Number of nodes
-	order int // Number of levels
-}
-
 func (b *BinaryTree) Size() int {
 	return b.size
 }
@@ -47,7 +45,6 @@ func (b *BinaryTree) Size() int {
 func (b *BinaryTree) Order() int {
 	return b.order
 }
-
 
 func (b *BinaryTree) uniqueValue(val interface{}) bool {
 	if b.root == nil {
@@ -136,8 +133,6 @@ func (b *BinaryTree) Exists(v interface{}) bool {
 	}
 }
 
-
-
 func (b *BinaryTree) deleteDeepestRightMostNode(drn *Node) {
 	var q []*Node
 	q = append(q, b.root)
@@ -217,49 +212,3 @@ func (b *BinaryTree) Print() {
 	}
 }
 
-
-/*
-
-func main() {
-	var t BinaryTree
-	t.Delete(5)
-	t.Print()
-	t.Insert(1)
-	t.Insert(2)
-	t.Insert(3)
-	t.Insert(3)
-	t.Insert(4)
-	t.Insert("4")
-	t.Insert(5)
-	t.Insert(6)
-	t.Insert(6)
-	t.Insert(7)
-	t.Insert(8)
-	t.Insert(8)
-	t.Insert(9)
-	t.Insert(10)
-	t.Insert(11)
-	t.Insert(12)
-	t.Insert(13)
-	t.Insert(14)
-	t.Insert(15)
-	t.Print()
-	t.Exists(3)
-	t.Exists(9)
-	t.Exists(1862534)
-	fmt.Println("Tree Size:", t.Size())
-	t.Delete(1)
-	t.Delete(2)
-	t.Delete(3)
-	t.Delete(4)
-	t.Delete(5)
-	t.Delete(1)
-	t.Print()
-	fmt.Println("Tree Size:", t.Size())
-
-	t.Each(func(val interface{}) {
-		fmt.Println("Element:", val)
-	})
-}
-
-*/
